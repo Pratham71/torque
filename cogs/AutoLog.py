2ee75db2b37119af8e14f8c3e38eebf644f37a47
Pratham <127861601+Pratham71@users.noreply.github.com>
2023-06-17 01:46:46 +0530

    nm

--- cogs/AutoLog.py
+++ cogs/AutoLog.py
@@ -24,6 +24,7 @@ class AutoLog(commands.Cog):
     whitelist=[1102927006703829014,882471259231887390,746023730488148091,1102164592974639155,1102927006703829014,746023730488148091,1118237629561962618]
     server_id=guild.id
     if server_id in whitelist:
+      global channel2
       log_channel_check=discord.utils.get(guild.channels,name='log-channel')
       moderation_command_channel_check=discord.utils.get(guild.channels,name='mod-logs')
 
@@ -36,7 +37,7 @@ class AutoLog(commands.Cog):
         channel = await guild.create_text_channel('log-channel',overwrites=overwrites)
         channel1 = await guild.create_text_channel('mod-logs',overwrites=overwrites)
         channel2 =  await guild.create_text_channel('spam-logs',overwrites=overwrites)
-
+        print(channel2)
         await channel.send('```THIS CHANNEL HAS BEEN SET FOR AUTO LOGGING!```\n@here')
         await channel1.send('```THIS CHANNEL HAS BEEN SET FOR MOD LOGS ANY COMMAND USED IN THIS SERVER WILL BE LOGGED HERE!```\n@here')
         await channel2.send('```THIS CHANNEL HAS BEEN SET FOR SPAM LOGS!```\n@here')
@@ -85,7 +86,12 @@ class AutoLog(commands.Cog):
 
       await log_channel.send(embed=event_embed)
   
+  @commands.Cog.listener()
+  async def on_message_delete(self,message):
+    log_channel=discord.utils.get(message.guild.channels,name='spam-logs')
 
-      
+    embed=discord.Embed(title=f'**Message deleted by** @{message.author.name}#{message.author.discriminator} **in channel** {message.channel.mention}',description=f'```{message.content}```',color=discord.Color.magenta())
+    await log_channel.send(embed=embed)
+  
 async def setup(client):
   await client.add_cog(AutoLog(client))
